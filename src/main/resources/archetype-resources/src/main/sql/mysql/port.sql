86e6b1c7ca22820c1940bdd002236f66e2198b2b
trumbitta <trumbitta@gmail.com>
2013-01-25 17:25:04 +0100

    Adapt to the Components Architecture

--- src/main/resources/archetype-resources/src/main/sql/mysql/port.sql
+++
@@ -1,5 +0,0 @@
-/*!40000 ALTER TABLE `pagemodels` DISABLE KEYS */;
-LOCK TABLES `pagemodels` WRITE;
-INSERT INTO `EntandoPort`.`pagemodels` VALUES ('${artifactId}','${pagemodel-human-readable-description}','<frames>\n	<frame pos="0">\n		<descr>Sample I</descr>\n	</frame>\n</frames>',NULL);
-UNLOCK TABLES;
-/*!40000 ALTER TABLE `pagemodels` ENABLE KEYS */;
--- src/main/resources/archetype-resources/src/main/sql/postgresql/port.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO pagemodels (code, descr, frames, plugincode) VALUES ('${artifactId}', '${pagemodel-human-readable-description}', '<frames>
-	<frame pos="0">
-		<descr>Sample I</descr>
-	</frame>
-</frames>', NULL);
